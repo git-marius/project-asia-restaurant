d82dbf2419c54d652fda399d4c5257789c2a29ed
Marius Güldner <eikengames@gmail.com>
2026-02-11 11:06:46 +0100

    updated model

--- python/app/models/measurements.py
+++ python/app/models/measurements.py
@@ -4,18 +4,18 @@ from datetime import datetime
 from app.extensions.db import db
 
 
-class RoomData(db.Model):
-    __tablename__ = 'room_data'  # table name in database
+class Measurements(db.Model):
+    __tablename__ = 'measurements'
 
     id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.Float, nullable=False)
     humidity = db.Column(db.Float, nullable=False)
     voc = db.Column(db.Float, nullable=False)
     motion = db.Column(db.Boolean, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
     def __repr__(self):
-        return f"<RoomData id={self.id} temperatur={self.temperatur} luftfeuchtigkeit={self.luftfeuchtigkeit} voc={self.voc} bewegung={self.bewegung} timestamp={self.timestamp}>"
+        return f"<Measurements id={self.id} temperatur={self.temperatur} luftfeuchtigkeit={self.luftfeuchtigkeit} voc={self.voc} bewegung={self.bewegung} timestamp={self.timestamp}>"
 
 
 
