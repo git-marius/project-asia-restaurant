0d202d4cb8b5d1ff0163250aa158d412dc9766c0
Marius Güldner <eikengames@gmail.com>
2026-02-17 08:33:53 +0100

    added persons and updated timestamp

--- python/app/models/measurements.py
+++ python/app/models/measurements.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from app.extensions.db import db
-
+from datetime import datetime, timezone
 
 class Measurements(db.Model):
     __tablename__ = 'measurements'
@@ -11,8 +11,11 @@ class Measurements(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     humidity = db.Column(db.Float, nullable=False)
     voc = db.Column(db.Float, nullable=False)
-    motion = db.Column(db.Boolean, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    persons = db.Column(db.Integer, nullable=False)
+    radar = db.Column(db.Boolean, nullable=False)
+    timestamp = db.Column(db.DateTime(timezone=True),
+                      default=lambda: datetime.now(timezone.utc),
+                      nullable=False)
 
     def __repr__(self):
         return f"<Measurements id={self.id} temperatur={self.temperatur} luftfeuchtigkeit={self.luftfeuchtigkeit} voc={self.voc} bewegung={self.bewegung} timestamp={self.timestamp}>"
